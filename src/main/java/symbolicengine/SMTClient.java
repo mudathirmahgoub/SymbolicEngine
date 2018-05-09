1c60d40a9973ae6fba65a7429775c765ed53cd35
mudathirmahgoub <mudathirmahgoub@gmail.com>
2018-05-08 19:19:38 -0500

    SMtClient

---
+++ src/main/java/symbolicengine/SMTClient.java
@@ -0,0 +1,42 @@
+package symbolicengine;
+
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class SMTClient
+{
+    private Process process;
+    private Scanner scanner;
+    private Formatter formatter;
+
+    public void connect() throws IOException
+    {
+
+        String command = "src/resources/z3.exe -in";
+
+        Runtime runtime = Runtime.getRuntime();
+        process = runtime.exec(command);
+        scanner = new Scanner(process.getInputStream());
+    }
+
+    public void sendCommand(String command) throws IOException
+    {
+        byte [] commandBytes = command.getBytes();
+        process.getOutputStream().write(commandBytes);
+        process.getOutputStream().flush();
+
+    }
+
+    public String getOutput() throws IOException
+    {
+        String line = scanner.nextLine();
+        return line;
+    }
+
+
+    public void disconnect()
+    {
+        process.destroyForcibly();
+    }
+}
---
+++ src/test/java/symbolicengine/SMTClientTest.java
@@ -0,0 +1,54 @@
+package symbolicengine;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SMTClientTest {
+
+    @Test
+    void connect() throws IOException
+    {
+        SMTClient client = new SMTClient();
+        client.connect();
+    }
+
+    @Test
+    void sendCommand() throws IOException
+    {
+        SMTClient client = new SMTClient();
+        client.connect();
+        client.sendCommand("(echo \"starting Z3...\")");
+    }
+
+    @Test
+    void getOutput() throws IOException
+    {
+        SMTClient client = new SMTClient();
+        client.connect();
+        client.sendCommand("(echo \"starting Z3...\")");
+        String output = client.getOutput();
+        assertEquals("starting Z3...", output);
+    }
+
+    @Test
+    void disconnect() throws IOException {
+        SMTClient client = new SMTClient();
+        client.connect();
+        client.sendCommand("(echo \"starting Z3...\")");
+        client.disconnect();
+    }
+
+    @Test
+    void simple() throws IOException
+    {
+        SMTClient client = new SMTClient();
+        client.connect();
+        client.sendCommand("(assert true)");
+        client.sendCommand("(check-sat)");
+        String output = client.getOutput();
+        assertEquals("sat", output);
+    }
+}
